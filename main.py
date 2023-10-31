0724e5c84c5cddbbd7930ae60d5602b3d9861db7
Roi Lavie <68992144+roisec@users.noreply.github.com>
2023-10-31 22:32:45 +0200

    init commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import requests,json,os
+s = requests.session()
+bot_token = os.getenv('TELEGRAM_BOT_TOKEN')
+chat_id = os.getenv('TELEGRAM_CHAT_ID')
+# print(bot_token)
+item_numbers = ["MTV53HX/A", "MTV63HX/A", "MTV43HX/A", "MTV43HX/A"]
+for item in item_numbers:
+    a = s.post("https://www.idigital.co.il/AppServices/SVClientApplicationJSONWSContent.asmx/GetBranchesForAvailability", json={"itemNo": item})
+    result = json.loads(a.text)
+    rs1 = result['d'][17]['Title']+": AvailabilInStock: "+str(result['d'][17]['AvailabilInStock'])
+    s.get("https://api.telegram.org/bot"+bot_token+"/sendMessage?chat_id="+chat_id+"&text="+rs1)
+    rs1 = result['d'][18]['Title']+": AvailabilInStock: "+str(result['d'][17]['AvailabilInStock'])
+    s.get("https://api.telegram.org/bot"+bot_token+"/sendMessage?chat_id="+chat_id+"&text="+rs1)
+
