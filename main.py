83ad7fa324791b23ded9cbef668ce7afe277ec86
Roi Lavie <68992144+roisec@users.noreply.github.com>
2023-11-07 08:54:59 +0200

    add store

--- main.py
+++ main.py
@@ -7,8 +7,10 @@ item_numbers = ["MTV53HX/A", "MTV63HX/A", "MTV43HX/A", "MTV43HX/A"]
 for item in item_numbers:
     a = s.post("https://www.idigital.co.il/AppServices/SVClientApplicationJSONWSContent.asmx/GetBranchesForAvailability", json={"itemNo": item})
     result = json.loads(a.text)
-    rs1 = result['d'][17]['Title']+": AvailabilInStock: "+str(result['d'][17]['AvailabilInStock'])
+    rs1 = "Idigital: " +result['d'][17]['Title']+": AvailabilInStock: "+str(result['d'][17]['AvailabilInStock'])
     s.get("https://api.telegram.org/bot"+bot_token+"/sendMessage?chat_id="+chat_id+"&text="+rs1)
-    rs1 = result['d'][18]['Title']+": AvailabilInStock: "+str(result['d'][17]['AvailabilInStock'])
+    rs1 = "Idigital: " +result['d'][18]['Title']+": AvailabilInStock: "+str(result['d'][18]['AvailabilInStock'])
+    s.get("https://api.telegram.org/bot"+bot_token+"/sendMessage?chat_id="+chat_id+"&text="+rs1)
+    rs1 = "Idigital: " +result['d'][7]['Title']+": AvailabilInStock: "+str(result['d'][7]['AvailabilInStock'])
     s.get("https://api.telegram.org/bot"+bot_token+"/sendMessage?chat_id="+chat_id+"&text="+rs1)
 
